5fedb18bd992c9f5f1ce96b2f90b05b86e2cd76e
Idris Babay <idris.i.babay@deutschebahn.com>
2025-10-28 15:33:44 +0100

    refactor format_message in utils

--- utils.py
+++ utils.py
@@ -15,3 +15,20 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 def prompt_gemini(msg: str, model_id: str = "gemini-2.5-flash") -> str:
     response = client.models.generate_content(model=model_id, contents=msg)
     return response.text
+
+def format_message(history: list[str]) -> str:
+    """Formats the message history for the LLM."""
+    # get system messages, since they provide important context
+    sys_mesages = [m for m in history if m["sender"] == "system"]
+    non_sys_messages = [m for m in history if m["sender"] != "system"]
+    # prepend system messages to the formatted history
+    formatted_history = "\n".join([
+        f"{m['sender']}: {m['content']}"
+        for m in sys_mesages
+    ])
+    # append the rest of the messages
+    formatted_history += "\n" + "\n".join([
+        f"{m['sender']}: {m['content']}"
+        for m in non_sys_messages
+    ])
+    return formatted_history
