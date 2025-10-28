6e831635d6ccebe25b6296bb96ff98955ffadf97
Idris Babay <idris.i.babay@deutschebahn.com>
2025-10-28 14:59:58 +0100

    add default value to model_id

--- utils.py
+++ utils.py
@@ -12,6 +12,6 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-def prompt_gemini(model_id: str, msg: str) -> str:
+def prompt_gemini(msg: str, model_id: str = "gemini-2.5-flash") -> str:
     response = client.models.generate_content(model=model_id, contents=msg)
     return response.text
